098cb963b9e584156949d758bbe71baa6e1792e9
nakata <c0b2310742@edu.teu.ac.jp>
2024-11-19 16:56:58 +0900

    演習問題2

--- pygame_image.py
+++ pygame_image.py
@@ -15,25 +15,29 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 400, 300
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         key_lst = pg.key.get_pressed()
+        move_x, move_y = 0, 0
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1))
+            move_y -= 1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, 1))
+            move_y += 1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1, 0))
+            move_x += 2
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 1))
-
+            move_x -= 2
         else:
-            kk_rct.move_ip((-1,0))  # 何も押されていない場合は左に流れる
+            move_x -= 1  # 何も押されていない場合は左に流れる
 
-        x = -(tmr%3200) #練習6-2
+        kk_rct.move_ip(move_x, move_y)
 
+
+        x = -(tmr%3200)
+    
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img2, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
@@ -42,7 +46,6 @@ def main():
         pg.display.update()
         tmr += 1        
         clock.tick(200)
-
 
 if __name__ == "__main__":
     pg.init()
